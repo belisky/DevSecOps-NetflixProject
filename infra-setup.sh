637baebd183aeab2bb4c9bef4fd807b1b62f7f7a
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-03-10 19:57:24 +0000

    chore: tries to add nvm api key

---
+++ infra-setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#installing java prior to jenkins
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre -y
+java -version
+ 
+
+#Installing Jenkins
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+#Installing docker
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+#run docker with sudo command.
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+#TMDB API KEY=92f88c1d3eeb2d32679b8a0f81ff3875
+#NVD API KEY=F28A42E3-08DF-EE11-833F-124ABD0694B9
+
+#Installing Trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy
+
+
+#sonar qube docker container installation.
+docker run -d --name sonar -p 8090:9000 sonarqube:lts-community
+
